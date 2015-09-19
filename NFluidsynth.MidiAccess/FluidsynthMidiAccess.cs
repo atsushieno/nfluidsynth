8d320233e0a02d41e5d3212aeb6cad529260af25
Atsushi Eno <atsushieno@gmail.com>
2015-09-19 19:21:59 +0800

    add managed-midi as submodule and MidiAccess implementation.

---
+++ NFluidsynth.MidiAccess/FluidsynthMidiAccess.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commons.Music.Midi;
+
+namespace NFluidsynth.MidiManager
+{
+	public class FluidsynthMidiAccess : IMidiAccess
+	{
+		public FluidsynthMidiAccess ()
+		{
+			Soundfonts = new List<string> ();
+			output = new FluidsynthMidiOutput (this);
+		}
+
+		public IEnumerable<IMidiInput> Inputs {
+			get { return Enumerable.Empty<IMidiInput> (); }
+		}
+
+		public IEnumerable<IMidiOutput> Outputs {
+			get { return Enumerable.Repeat (output, 1); }
+		}
+
+		public IList<string> Soundfonts { get; private set; }
+
+		public event EventHandler<MidiConnectionEventArgs> StateChanged; // won't detect...
+
+		FluidsynthMidiOutput output;
+	}
+
+	class FluidsynthMidiOutputDetails : IMidiPortDetails
+	{
+		public string Id {
+			get { return "id0"; }
+		}
+
+		public string Manufacturer {
+			get { return "fluidsynth project"; }
+		}
+
+		public string Name {
+			get { return "Fluidsynth output port"; }
+		}
+
+		public string Version {
+			get { return "v0.000"; }
+		}
+	}
+
+	class FluidsynthMidiOutput : IMidiOutput
+	{
+		static Task completed_task = Task.FromResult (false);
+
+		public FluidsynthMidiOutput (FluidsynthMidiAccess midiAccess)
+		{
+			if (midiAccess == null)
+				throw new ArgumentNullException ("midiAccess");
+			midi_access = midiAccess;
+			Details = new FluidsynthMidiOutputDetails ();
+		}
+
+		public MidiPortConnectionState Connection { get; private set; }
+
+		public IMidiPortDetails Details { get; private set; }
+
+		public MidiPortDeviceState State { get; private set; }
+
+		// it won't change...
+		public event EventHandler StateChanged;
+
+		FluidsynthMidiAccess midi_access;
+
+		public IList<string> Soundfonts {
+			get { return midi_access.Soundfonts; }
+		}
+
+		public Task CloseAsync ()
+		{
+			if (synth != null)
+				synth.Dispose ();
+			synth = null;
+			if (settings != null)
+				settings.Dispose ();
+			settings = null;
+			return Task.FromResult (true);
+		}
+
+		public void Dispose ()
+		{
+			CloseAsync ().Wait ();
+		}
+
+		Settings settings;
+		Synth synth;
+
+		public Task OpenAsync ()
+		{
+			if (settings != null)
+				throw new InvalidOperationException ("The MIDI output is already open.");
+			settings = new Settings ();
+			synth = new Synth (settings);
+
+			return Task.FromResult (true);
+		}
+
+		public Task SendAsync (byte [] msg, int length, long timestamp)
+		{
+			int ch = msg [0] & 0x0F;
+			switch (msg [0] & 0xF0) {
+			case 0x80:
+				synth.NoteOff (ch, msg [1]);
+				break;
+			case 0x90:
+				if (msg [2] == 0)
+					synth.NoteOff (ch, msg [1]);
+				else
+					synth.NoteOn (ch, msg [1], msg [2]);
+				break;
+			case 0xA0:
+				// No PAf in fluidsynth?
+				break;
+			case 0xB0:
+				synth.CC (ch, msg [1], msg [2]);
+				break;
+			case 0xC0:
+				synth.ProgramChange (ch, msg [1]);
+				break;
+			case 0xD0:
+				synth.ChannelPressure (ch, msg [1]);
+				break;
+			case 0xE0:
+				synth.PitchBend (ch, (msg [1] << 14) + msg [2]);
+				break;
+			case 0xF0:
+				synth.Sysex (new ArraySegment<byte> (msg, 0, length).ToArray (), null);
+				break;
+			}
+			return completed_task;
+		}
+	}
+}
+
--- NFluidsynth.Sample/Program.cs
+++ NFluidsynth.Sample/Program.cs
@@ -10,14 +10,18 @@ namespace NFluidsynth.Sample
 		public static void Main (string[] args)
 		{
 			using (var settings = new Settings ()) {
+				// Change this if you don't have pulseaudio or want to change to anything else.
 				if (Environment.OSVersion.Platform == PlatformID.Unix)
-					settings [ConfigurationKeys.AudioDriver].StringValue = "alsa";
+					settings [ConfigurationKeys.AudioDriver].StringValue = "pulseaudio";
+				settings [ConfigurationKeys.SynthAudioChannels].IntValue = 2;
 				using (var syn = new Synth (settings)) {
 					foreach (var arg in args)
 						if (Synth.IsSoundFont (arg))
 							syn.LoadSoundFont (arg, true);
 					if (syn.FontCount == 0)
 						syn.LoadSoundFont ("/usr/share/sounds/sf2/FluidR3_GM.sf2", true);
+					for (int i = 0; i < 16; i++)
+						syn.SoundFontSelect (i, 0);
 					var files = args.Where (a => Synth.IsMidiFile (a));
 					if (files.Any ()) {
 						foreach (var arg in files) {
@@ -31,7 +35,8 @@ namespace NFluidsynth.Sample
 						}
 					} else {
 						using (var adriver = new AudioDriver (syn.Settings, syn)) {
-							syn.NoteOn (0, 60, 100);
+							syn.ProgramChange (0, 1);
+							syn.NoteOn (0, 60, 120);
 							Thread.Sleep (5000);
 							syn.NoteOff (0, 60);
 						}
