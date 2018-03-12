08527ba590e4e61a9512770d25387c67724d8b52
Atsushi Eno <atsushieno@gmail.com>
2018-03-12 17:02:53 +0900

    switch to the latest fluidsynth-based API, namely its newest sfloader API.
    For Android support we had to extend fluidsynth to provide custom sfloader
    functionality for loading sf from any stream. Now fluidsynth itself has
    its own extensibility, we could use that.
    
    In fact fluidsynth-midi-service doesn't work fine with this yet.

--- NFluidsynth/NFluidsynth/SoundFontLoader.cs
+++ NFluidsynth/NFluidsynth/SoundFontLoader.cs
@@ -1,23 +1,60 @@
 ﻿using System;
 using NFluidsynth.Native;
+using static NFluidsynth.Native.LibFluidsynth;
 
 namespace NFluidsynth
 {
-	public class SoundFontLoader : FluidsynthObject
+	public abstract class SoundFontLoader : IDisposable
 	{
-		public SoundFontLoader (Synth synth, StreamLoader streamLoader)
-			: base (LibFluidsynth.Misc.fluid_synth_new_stream_sfloader (synth.Handle, streamLoader.Handle), true)
+		public SoundFontLoader (Synth synth)
+		{
+			i = new SoundFontLoaderInternal (synth, (p, f) => Load (f), p => Free ());
+		}
+
+		SoundFontLoaderInternal i;
+
+		public void Dispose ()
+		{
+			if (i != null)
+				i.Dispose ();
+			i = null;
+		}
+
+		internal IntPtr Handle {
+			get { return i.Handle; }
+		}
+
+		public abstract IntPtr Load (string filename);
+
+		public abstract void Free ();
+
+		public abstract IntPtr Open (string filename);
+		public abstract int Read (IntPtr buf, long count, IntPtr handle);
+		public abstract int Seek (IntPtr handle, long position, int origin); // I avoid System.IO-specific enum here.
+		public abstract int Tell (IntPtr handle);
+		public abstract int Close (IntPtr handle);
+	}
+	
+	class SoundFontLoaderInternal : FluidsynthObject
+	{
+		public SoundFontLoaderInternal (Synth synth, SfLoader.fluid_sfloader_load_t load, SfLoader.fluid_sfloader_free_t free)
+			: base (SfLoader.new_fluid_sfloader (load, free), true)
 		{
 		}
 
-		public SoundFontLoader (IntPtr handle)
+		public SoundFontLoaderInternal (IntPtr handle)
 			: base (handle, false)
 		{
 		}
 
 		protected override void OnDispose ()
 		{
-			LibFluidsynth.Misc.fluid_synth_delete_stream_sfloader (Handle);
+			SfLoader.delete_fluid_sfloader (Handle);
+		}
+
+		public void SetCallbacks (SoundFontLoader loader)
+		{
+			SfLoader.fluid_sfloader_set_callbacks (Handle, loader.Open, (b, l, p) => loader.Read (b, l, p), (p, i, o) => loader.Seek (p, i, o), p => loader.Tell (p), p => loader.Close (p));
 		}
 	}
 }
