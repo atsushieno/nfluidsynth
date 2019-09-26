1b5094c7988dff1cf95fbfbc7b2aa5bffeb54ce3
zumorica <zddm@outlook.es>
2019-09-26 12:03:07 +0200

    Fix two crashes

--- NFluidsynth/Native/LibFluidsynth.Misc.cs
+++ NFluidsynth/Native/LibFluidsynth.Misc.cs
@@ -6,11 +6,11 @@ namespace NFluidsynth.Native
     internal static partial class LibFluidsynth
     {
         [DllImport(LibraryName)]
-        [return: MarshalAs(UnmanagedType.I4)]
+        [return: MarshalAs(UnmanagedType.I1)]
         internal static extern bool fluid_is_soundfont([MarshalAs(UnmanagedType.LPUTF8Str)] string filename);
 
         [DllImport(LibraryName)]
-        [return: MarshalAs(UnmanagedType.I4)]
+        [return: MarshalAs(UnmanagedType.I1)]
         internal static extern bool fluid_is_midifile([MarshalAs(UnmanagedType.LPUTF8Str)] string filename);
 
         [DllImport(LibraryName)]
