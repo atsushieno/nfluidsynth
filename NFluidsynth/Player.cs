e0327b2ae6b884b59b36414fc5035e460d2cefed
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-05-21 02:09:09 +0200

    Add missing ThrowIfDisposed to Player.

--- NFluidsynth/Player.cs
+++ NFluidsynth/Player.cs
@@ -25,12 +25,14 @@ namespace NFluidsynth
 
         public void Add(string midifile)
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_add(Handle, midifile);
         }
 
         #if NET472 || NETCOREAPP
         public unsafe void AddMem(ReadOnlySpan<byte> buffer)
         {
+            ThrowIfDisposed();
             fixed (byte* ptr = buffer)
                 AddMem ((IntPtr) ptr, buffer.Length);
         }
@@ -38,64 +40,104 @@ namespace NFluidsynth
         
         public unsafe void AddMem(byte [] buffer, int offset, int length)
         {
+            ThrowIfDisposed();
             fixed (byte* ptr = buffer)
                 AddMem ((IntPtr) (ptr + offset), buffer.Length);
         }
 
         public void AddMem(IntPtr buffer, int length)
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_add_mem(Handle, buffer, length);
         }
 
         public void Play()
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_play(Handle);
         }
 
         public void Stop()
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_stop(Handle);
         }
 
         public void Join()
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_join(Handle);
         }
 
         public void SetLoop(int loop)
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_set_loop(Handle, loop);
         }
 
         [Obsolete("Use " + nameof(MidiTempo) + " property instead.")]
         public void SetTempo(int tempo)
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_set_midi_tempo(Handle, tempo);
         }
 
         [Obsolete("Use " + nameof(Bpm) + " property instead.")]
         public void SetBpm(int bpm)
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_set_bpm(Handle, bpm);
         }
 
-        public int CurrentTick => LibFluidsynth.fluid_player_get_current_tick(Handle);
-        public int GetTotalTicks => LibFluidsynth.fluid_player_get_total_ticks(Handle);
+        public int CurrentTick
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return LibFluidsynth.fluid_player_get_current_tick(Handle);
+            }
+        }
+
+        public int GetTotalTicks
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return LibFluidsynth.fluid_player_get_total_ticks(Handle);
+            }
+        }
 
         public int Bpm
         {
-            get => LibFluidsynth.fluid_player_get_bpm(Handle);
-            set => LibFluidsynth.fluid_player_set_bpm(Handle, value);
+            get
+            {
+                ThrowIfDisposed();
+                return LibFluidsynth.fluid_player_get_bpm(Handle);
+            }
+            set
+            {
+                ThrowIfDisposed();
+                LibFluidsynth.fluid_player_set_bpm(Handle, value);
+            }
         }
 
         public int MidiTempo
         {
-            get => LibFluidsynth.fluid_player_get_midi_tempo(Handle);
-            set => LibFluidsynth.fluid_player_set_midi_tempo(Handle, value);
+            get
+            {
+                ThrowIfDisposed();
+                return LibFluidsynth.fluid_player_get_midi_tempo(Handle);
+            }
+            set
+            {
+                ThrowIfDisposed();
+                LibFluidsynth.fluid_player_set_midi_tempo(Handle, value);
+            }
         }
 
         public unsafe void SetPlaybackCallback(MidiEventHandler handler)
         {
+            ThrowIfDisposed();
             LibFluidsynth.fluid_player_set_playback_callback(Handle,
                 Utility.PassDelegatePointer<LibFluidsynth.handle_midi_event_func_t>(
                     (d, e) =>
@@ -108,6 +150,13 @@ namespace NFluidsynth
             _handler = b;
         }
 
-        public FluidPlayerStatus Status => LibFluidsynth.fluid_player_get_status(Handle);
+        public FluidPlayerStatus Status
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return LibFluidsynth.fluid_player_get_status(Handle);
+            }
+        }
     }
 }
